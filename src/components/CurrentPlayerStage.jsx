bb7d976dbd7410deeba2d75d182f4a0dc9edee50
HYEOKJUNCHOI <gurwns369@naver.com>
2026-05-10 23:59:47 +0900

    Revert "style: reposition income badges near passive and interest"
    This reverts commit 4961ea2e49f0541bcdc1f4a988817dc217504fd8.

--- src/components/CurrentPlayerStage.jsx
+++ src/components/CurrentPlayerStage.jsx
@@ -392,7 +392,6 @@ export default function CurrentPlayerStage({
                 player={player}
                 forceAllPassives
               />
-              <RentIncomeChip value={aptIncome || 80} />
             </div>
 
           <div className="col-span-2 -mt-1 flex min-w-0 items-center gap-1.5 overflow-visible">
